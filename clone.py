77a90d38e3fe32dea4fdde0008ba44db6bd89db3
Nathan Greco <nathan.greco@gmail.com>
2017-06-14 21:50:02 -0400

    Added flipped data set

--- clone.py
+++ clone.py
@@ -6,9 +6,19 @@ import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 
+#Double set with flipped images
+def add_flipped_set(X, y):
+	#Check for a valid data set
+	assert(len(X) == len(y))
+
+	#Add flipped values
+	for i in range(len(X)):	
+		np.append(X, cv2.flip(X[i], 1))
+		np.append(y, (y[i] * -1.0))
+	return
 
 #Get training data
 lines = []
@@ -31,16 +41,19 @@ for line in lines:
 	measurement = float(line[3])
 	measurements.append(measurement)
 
+#Add flipped set
 X_train = np.array(images)
 y_train = np.array(measurements)
+add_flipped_set(X_train, y_train)
+print(y_train[300], ',', y_train[len(lines) + 300])
 
 #Create Model
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(Conv2D(6, (5, 5), activation="relu"))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation="relu"))
+model.add(Conv2D(6, (5, 5), activation="relu"))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
@@ -48,7 +61,7 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 
 model.save('model.h5')
 exit()
