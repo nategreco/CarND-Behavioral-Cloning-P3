394535496a6f805d0a390a4affa2ed748e0a4e9c
Nathan Greco <nathan.greco@gmail.com>
2017-06-26 18:44:07 -0400

    Changed convolutional layer dropouts to spatialdropout2d

--- networks.py
+++ networks.py
@@ -43,10 +43,10 @@ def create_nvidia_model(rows, cols, channels):
 	model.add(Cropping2D(cropping=((69, 15), (0, 0))))				#76x320x3
 	model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))#36x158x24
 	model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))#16x77x36
-	model.add(Dropout(0.5))											#Dropout
+	model.add(SpatialDropout2D(0.2))								#2D-Dropout
 	model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))#6x37x48
 	model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))#4x35x64
-	model.add(Dropout(0.5))											#Dropout
+	model.add(SpatialDropout2D(0.2))								#Dropout
 	model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))#2x33x64
 	model.add(Flatten())											#4224x1
 	model.add(Dense(100))											#100x1
